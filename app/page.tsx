c8ba369dccba1ed29c784552e9697c557fae7670
Samandar-Jumanov <samandarjumanov055@gmail.com>
2024-04-05 16:03:04 +0500

    adding donation

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
 import { HeadContent } from "@/components/Head";
 import { MostCommonCourses } from "@/components/CommonCourses"
 import Footer from "@/components/Footer"
+import DonateComponent from "@/components/Donation";
+
 export default function Home() {
   return (
         <div >
            <HeadContent />
            <MostCommonCourses />
+           <div className="container mx-auto px-4">
+           <DonateComponent />
+          </div>
            <Footer/>
+
         </div>
   );
 }
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,19 +7,16 @@ const Footer = () => {
   return (
     <footer className="bg-slate-500 w-full  p-4 flex flex-col items-center gap-4">
       <div className="flex flex-col md:flex-row items-center justify-center gap-4">
-        {/* Instagram Link */}
         <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2">
           <FaInstagram size={24} />
           <span>Instagram</span>
         </a>
         
-        {/* Phone Number */}
         <a href="tel:+1234567890" className="flex items-center gap-2">
           <FaPhone size={24} />
           <span>+1 234 567 890</span>
         </a>
         
-        {/* YouTube Link */}
         <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2">
           <FaYoutube size={24} />
           <span>YouTube</span>
